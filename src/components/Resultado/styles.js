567db9dfcbad45f3ed9d8f1eea48403f5ebe5ebd
Uriel Cristino <urielcris.97@gmail.com>
2021-08-16 14:56:19 -0500

    Creamos los stylescomponents para Resultado

---
+++ src/components/Resultado/styles.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const Mensaje = styled.p`
+   background-color: rgb(127, 224, 237);
+   margin-top: 2rem;
+   padding: 1rem;
+   text-align: center;
+`;
+
+export const ResultadoCotizacion = styled.div`
+   text-align: center;
+   padding: .5rem;
+   border: 1px solid #26C6DA;
+   background-color:  rgb(127, 224, 237);
+   position: relative;
+`
+
+export const TextoCotizacion = styled.p`
+   color: #00838F;
+   padding: 1rem;
+   text-transform: uppercase;
+   font-weight: bold;
+   margin: 0;
+`;
